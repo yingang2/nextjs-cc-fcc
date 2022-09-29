40c7711939905afb60d4189d98887fa4a67d19b5
yingang2 <277123050@qq.com>
2022-09-30 00:36:25 +0800

    delete out

--- out/_next/static/571TcwF1E3y2fd01TXuGJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0615f411-5d86f980b8498d50.js","static/css/7c9e31c2156353a4.css","static/chunks/pages/index-c733594a11f6f6b9.js"],"/_error":["static/chunks/pages/_error-a51993fe870ec2c8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
